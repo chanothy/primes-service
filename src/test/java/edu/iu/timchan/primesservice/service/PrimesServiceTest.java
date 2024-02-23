b8d5f805e500757efa07fc0b9f357c73d915fdf4
Tim Chan <timchan@iu.edu>
2024-02-22 20:35:49 -0500

    first commit

---
+++ src/test/java/edu/iu/timchan/primesservice/service/PrimesServiceTest.java
@@ -0,0 +1,33 @@
+package edu.iu.timchan.primesservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import edu.iu.timchan.primesservice.service.PrimesService;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimesServiceTest {
+
+    PrimesService primesService = new PrimesService();
+
+    @Test
+    void _45IsNotPrime() {
+        assertFalse(primesService.isPrime(45));
+    }
+
+    @Test
+    void _47IsPrime() {
+        assertTrue(primesService.isPrime(47));
+    }
+
+    @Test
+    void _549274982374972IsNotPrime() {
+        assertFalse(primesService.isPrime(549274982374972L));
+    }
+
+    @Test
+    void _285191IsPrime() {
+        assertTrue(primesService.isPrime(285191));
+    }
+
+}
